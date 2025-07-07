b97a6a8d1e036e2949c4a8cc8bd12125920c0431
AnthonyPortillaCano <anthony-nek29@outlook.com@github.com>
2025-07-07 00:55:31 -0500

    I added the new endpoint api

---
+++ CloudDemoApp/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudDemoApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet("ListProduct")]
+        public List<string> GetAll()
+        {
+            var products = new List<string>
+            {
+                "SODA",
+                "MILK"
+            };
+            return products;
+        }
+    }
+}
